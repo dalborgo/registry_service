8631d750bea24892253fa87bc04a66d4649a4fd9
dalborgo <marco@dalborgo.it>
2019-05-27 12:22:24 +0200

    first

--- src/model/index.js
+++ src/model/index.js
@@ -1,21 +1,35 @@
-import util from "util"
 import { hashSync, compare } from 'bcryptjs'
 export function export_model(ottoman, getFunctions){
   const _throw = m => {throw m}
-  const User = ottoman.model('User', {
+  const Registry = ottoman.model('REGISTRY', {
     username: {type: 'string', readonly: true},
-    email: 'string',
-    name: 'string',
-    role: 'string',
     password: 'string',
+    surname: 'string', //Se sesso=M,F
+    name: 'string', //Se sesso=M,F;PG
+    gender: 'string',//enum M=Maschio, F=Femmina, PG=Persona giuridica
+    birth_day: 'string',
+    birth_city: 'integer',//Restituito da getComuni
+    cf: 'string',//Se sesso=M,F
+    vat: 'string',//Se sesso=PG
+    nationality: 'string',//Restituito da File excel Tabelle (Cittadinanza)
+    address: 'string',
+    arress_number: 'string',
+    state: 'string',//Restituito da File excel Tabelle (Comuni)
+    city: 'string',//Restituito da File excel Tabelle (Comuni)
+    zip: 'string',
+    phone: 'string',
+    cellular: 'string',
+    email: 'string',
+    pec: 'string',//Se sesso=PG obbligatorio o PEC o codice_destinatario (per fattura elettronica AE)
+    sdi: 'string',
     createdAt: {type: 'Date', default: Date.now},
-    updateAt: 'Date'
+    updatedAt: 'Date'
   }, {
     id: 'username'
   })
-  getFunctions.call(User)
+  getFunctions.call(Registry)
 
-  User.pre('save', function (user, next) {
+  Registry.pre('save', function (user, next) {
     //fixme capire come non ricriptare la password
     if (this.password && this.password.length !== 60) {
       this.password = hashSync(this.password, 10)
@@ -24,20 +38,11 @@ export function export_model(ottoman, getFunctions){
     next()
   })
 
-  User.check_email = async email => await User.how_many({email}) && _throw('Duplicated: ' + email) //non ritorno niente ma serve l'await per testare il throw
+  Registry.check_username = async username => await Registry.how_many({username}) && _throw(`Duplicated: ${username}`, 'DUPUSERNAME')
+  Registry.check_email = async email => await Registry.how_many({email}) && _throw('Duplicated: ' + email) //non ritorno niente ma serve l'await per testare il throw
 
-  User.prototype.matchesPassword = function (password) {
+  Registry.prototype.matchesPassword = function (password) {
     return compare(password, this.password)
   }
-
-  User.prototype.commit = async function () {
-    const save = util.promisify(this.save.bind(this))
-    await save()
-  }
-
-  User.prototype.expand = async function (path) {
-    const load = util.promisify(this.load.bind(this))
-    await load(path)
-  }
-  return User
+  return Registry
 }
